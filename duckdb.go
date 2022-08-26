52f8cb0089a0d62301cb77f77760ac6014abd977
Benjamin Egelund-Müller <b@egelund-muller.com>
2022-08-26 11:48:21 +0200

    Add build flags for Windows compilation

--- duckdb.go
+++ duckdb.go
@@ -5,8 +5,10 @@
 package duckdb
 
 /*
-#cgo CXXFLAGS: -std=c++11 -fPIC -O3
+#cgo CXXFLAGS: -std=c++11 -fPIC -g0 -O3
+#cgo windows CXXFLAGS: -DWIN32 -DDUCKDB_BUILD_LIBRARY
 #cgo linux LDFLAGS: -ldl
+#cgo windows LDFLAGS: -lws2_32
 #include <duckdb.h>
 */
 import "C"
