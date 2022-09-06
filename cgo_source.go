b4f7d12fc1f327f6a214cc9f8ba2d5f4393e208b
Benjamin Egelund-Müller <b@egelund-muller.com>
2022-09-07 00:23:43 +0200

    Fix include path for compile from source

--- cgo_source.go
+++ cgo_source.go
@@ -3,7 +3,8 @@
 package duckdb
 
 /*
-#cgo CXXFLAGS: -std=c++11 -fPIC -g0 -O3 -I${SRCDIR}/deps/source/include
+#cgo CFLAGS: -I${SRCDIR}/deps/source/include
+#cgo CXXFLAGS: -std=c++11 -fPIC -g0 -O3
 #cgo windows CXXFLAGS: -DWIN32 -DDUCKDB_BUILD_LIBRARY
 #cgo linux LDFLAGS: -ldl
 #cgo windows LDFLAGS: -lws2_32
