d34a6f7de219897cdaf00c5d129a78b8fa3df1cb
Nick Pleatsikas <nick@pleatsikas.me>
2023-06-03 00:12:44 -0700

    Automatic Build Support for ARM on Linux (#96)
    * Added build support for ARM64 on Linux.
    
    * Removed go build rules restricting Linux builds to AMD64.
    * Added make target for arm64.
    * Added deps directory for arm64.
    * Add job to deps workflow to install cross compiler toolchain and
      run arm64 make target.
    * Fixed minor typo in rm command in commit job.
    
    * Re-build static libraries
    
    * Changed build rules to be more restrictive.
    
    * Limited Linux builds to amd64 and arm64.
    
    * Updated included library binaries.
    
    * Removed arm64 static library.
    * Checked out darwin static libraries from master to ensure that the
      commit doesn't modify or delete them.
    
    * Fixed typo in build rules.
    
    * Revert change to build rules.
    
    ---------
    
    Co-authored-by: MrFlynn <MrFlynn@users.noreply.github.com>

--- cgo_static.go
+++ cgo_static.go
@@ -1,4 +1,4 @@
-//go:build !(duckdb_use_lib || duckdb_from_source) && (darwin || (linux && amd64))
+//go:build !(duckdb_use_lib || duckdb_from_source) && (darwin || (linux && (amd64 || arm64)))
 
 package duckdb
 
@@ -7,6 +7,7 @@ package duckdb
 #cgo darwin,amd64 LDFLAGS: -lc++ -L${SRCDIR}/deps/darwin_amd64
 #cgo darwin,arm64 LDFLAGS: -lc++ -L${SRCDIR}/deps/darwin_arm64
 #cgo linux,amd64 LDFLAGS: -lstdc++ -lm -ldl -L${SRCDIR}/deps/linux_amd64
+#cgo linux,arm64 LDFLAGS: -lstdc++ -lm -ldl -L${SRCDIR}/deps/linux_arm64
 #include <duckdb.h>
 */
 import "C"
