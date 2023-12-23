7c5d8e055ee848f388afdcca14872ec4ddba17d9
Marc Boeker <marc@at6.net>
2023-12-23 17:50:44 +0100

    Add LDFLAGS for windows on amd64

--- cgo_static.go
+++ cgo_static.go
@@ -1,13 +1,13 @@
-//go:build !duckdb_use_lib && (darwin || (linux && (amd64 || arm64)))
+//go:build !duckdb_use_lib && (darwin || (linux && (amd64 || arm64)) || (windows && amd64))
 
 package duckdb
 
 /*
-#cgo LDFLAGS: -lduckdb
-#cgo darwin,amd64 LDFLAGS: -lc++ -L${SRCDIR}/deps/darwin_amd64
-#cgo darwin,arm64 LDFLAGS: -lc++ -L${SRCDIR}/deps/darwin_arm64
-#cgo linux,amd64 LDFLAGS: -lstdc++ -lm -ldl -L${SRCDIR}/deps/linux_amd64
-#cgo linux,arm64 LDFLAGS: -lstdc++ -lm -ldl -L${SRCDIR}/deps/linux_arm64
+#cgo darwin,amd64 LDFLAGS: -lduckdb -lc++ -L${SRCDIR}/deps/darwin_amd64
+#cgo darwin,arm64 LDFLAGS: -lduckdb -lc++ -L${SRCDIR}/deps/darwin_arm64
+#cgo linux,amd64 LDFLAGS: -lduckdb -lstdc++ -lm -ldl -L${SRCDIR}/deps/linux_amd64
+#cgo linux,arm64 LDFLAGS: -lduckdb -lstdc++ -lm -ldl -L${SRCDIR}/deps/linux_arm64
+#cgo windows,amd64 LDFLAGS: -lduckdb -lws2_32 -lstdc++ -Wl,-Bstatic -lpthread -lm -L${SRCDIR}/deps/windows_amd64
 #include <duckdb.h>
 */
 import "C"
