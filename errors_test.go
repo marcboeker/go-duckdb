18957dd9a4a804d340327bd6dd516c0d4f77f95c
apocelipes <seve3r@outlook.com>
2024-09-10 19:56:36 +0800

    add DUCKDB_INVALID_CONFIGURATION to duckdb.Error

--- errors_test.go
+++ errors_test.go
@@ -383,7 +383,7 @@ func TestGetDuckDBError(t *testing.T) {
 			Msg:  "Error: xxx",
 			Type: ErrorTypeUnknownType,
 		},
-		// next two for the prefix testing
+		// next 3 cases for the prefix testing
 		{
 			Msg:  "Invalid Error: xxx",
 			Type: ErrorTypeInvalid,
@@ -391,6 +391,10 @@ func TestGetDuckDBError(t *testing.T) {
 		{
 			Msg:  "Invalid Input Error: xxx",
 			Type: ErrorTypeInvalidInput,
+		},
+		{
+			Msg:  "Invalid Configuration Error: xxx",
+			Type: ErrorTypeInvalidConfiguration,
 		},
 	}
 
